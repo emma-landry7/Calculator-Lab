bc6d9601d0bff9bbfc13a0343a69fb8347e8ce11
emma-landry7 <emmablandry730@gmail.com>
2023-11-26 00:15:05 -0600

    changed form to buttons

--- calculator.js
+++ calculator.js
@@ -9,28 +9,45 @@
 
 // add(2,3)
 
-function calc(number1, number2, operation) {
-    let num1 = Number.parseFloat(number1)
-    let num2 = Number.parseFloat(number2)
-    let result = function result() {
-        switch (operation) {
-        case '+':
-            const add = num1 + num2;
-            return add;
-        case '-':
-            const subtract = num1 - num2;
-            return subtract;
-        case '*':
-            const multiply = num1 * num2;
-            return multiply;
-        case '/':
-            const divide = num1 / num2;
-            return divide;
-        };
-    }
-    // document.getElementById(result).innerHTML = calc(2,3,'+')
-    return result()
+// function calc(number1, number2, operation) {
+//     let num1 = document.getElementById("1")
+//     console.log(num1)
+//     let num2 = Number.parseFloat(number2)
+//     let result = function result() {
+//         switch (operation) {
+//         case '+':
+//             const add = num1 + num2;
+//             return add;
+//         case '-':
+//             const subtract = num1 - num2;
+//             return subtract;
+//         case '*':
+//             const multiply = num1 * num2;
+//             return multiply;
+//         case '/':
+//             const divide = num1 / num2;
+//             return divide;
+//         default:
+//             return num1 + num2;
+//         };
+//     }
+//     // document.getElementById(result).innerHTML = calc(2,3,'+')
+//     return result()
+// }
+
+// calc(1,1,'+')
+
+// console.log(calc(2,3,'*'))
+// document.getElementById(result).innerHTML = calc(2,3,'+')
+// document.getElementById("result").innerHTML = submit
+
+
+function display(val) {
+    document.getElementById("result").value += val
 }
 
-console.log(calc(2,3,'+'))
-// document.getElementById(result).innerHTML = calc(2,3,'+')
+
+function solve() {
+    let x = document.getElementById("result").value
+    // let y = function calculate()
+}
