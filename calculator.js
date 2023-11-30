c024f032daa334925c9cf06c5ab8c6261b34865b
emma-landry7 <emmablandry730@gmail.com>
2023-11-29 20:45:57 -0600

    completed calculator lab

--- calculator.js
+++ calculator.js
@@ -9,31 +9,28 @@
 
 // add(2,3)
 
-// function calc(number1, number2, operation) {
-//     let num1 = document.getElementById("1")
-//     console.log(num1)
-//     let num2 = Number.parseFloat(number2)
-//     let result = function result() {
-//         switch (operation) {
-//         case '+':
-//             const add = num1 + num2;
-//             return add;
-//         case '-':
-//             const subtract = num1 - num2;
-//             return subtract;
-//         case '*':
-//             const multiply = num1 * num2;
-//             return multiply;
-//         case '/':
-//             const divide = num1 / num2;
-//             return divide;
-//         default:
-//             return num1 + num2;
-//         };
-//     }
-//     // document.getElementById(result).innerHTML = calc(2,3,'+')
-//     return result()
-// }
+function calc(number1, number2, operation) {
+    let num1 = Number.parseFloat(number1)
+    // console.log(num1)
+    let num2 = Number.parseFloat(number2)
+    switch (operation) {
+    case '+':
+        const add = num1 + num2;
+        return add;
+    case '-':
+        const subtract = num1 - num2;
+        return subtract;
+    case '*':
+        const multiply = num1 * num2;
+        return multiply;
+    case '/':
+        const divide = num1 / num2;
+        return divide;
+    default:
+        return num1 + num2;
+    };
+    // document.getElementById(result).innerHTML = calc(2,3,'+')
+}
 
 // calc(1,1,'+')
 
@@ -43,11 +40,34 @@
 
 
 function display(val) {
-    document.getElementById("result").value += val
+    console.log(val)
+    document.getElementById("result").innerHTML += val
+    return val
 }
 
 
 function solve() {
-    let x = document.getElementById("result").value
-    // let y = function calculate()
+    let displayVal = document.getElementById("result").innerHTML
+    console.log(displayVal)
+    let operator
+    for (char of displayVal) {
+        console.log(char)
+        if (char === '+') {
+            operator = char 
+        } else if (char === "-") {
+            operator = char
+        } else if (char === "*") {
+            operator = char
+        } else if (char === "/") {
+            operator = char
+        }
+    }
+    const myArray = displayVal.split(operator)
+    console.log(myArray)
+    
+    document.getElementById("result").innerHTML = calc(myArray[0], myArray[1], operator)
+}
+
+function reset() {
+    document.getElementById("result").innerHTML = ''
 }
